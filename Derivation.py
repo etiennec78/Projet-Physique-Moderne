1816a1df43274437ba5278a3239ce0594fd17326
Etienne C. <59794011+etiennec78@users.noreply.github.com>
2026-06-12 12:48:22 +0200

    Add a function to double derive

--- Derivation.py
+++ Derivation.py
@@ -32,12 +32,29 @@ def derive(x_values: list, y_values: list, i: int) -> float:
     return (y_b - y_a) / (x_b - x_a)
 
 
+def second_derivative(x_values: list, y_values: list, i: int) -> float:
+    a = i - 1
+    b = i + 1
+
+    y_a = derive(x_values, y_values, a)
+    y_b = derive(x_values, y_values, b)
+
+    x_a = x_values[a]
+    x_b = x_values[b]
+
+    return (y_b - y_a) / (x_b - x_a)
+
+
 if __name__ == "__main__":
 
     # Deriving x²
     x_values = arange(1, 100, 0.1)
     y_values = power_function(x_values, 2)
     print(derive(x_values, y_values, 100))
+
+    # Double deriving x²
+    x_values = arange(1, 100, 0.1)
+    print(second_derivative(x_values, y_values, 100))
 
     # Deriving 2x
     y_values = 2 * x_values
