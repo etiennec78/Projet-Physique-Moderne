3ad448550a9a3d5b3316e60bac8b5c695de25ffa
Etienne C. <59794011+etiennec78@users.noreply.github.com>
2026-06-17 09:29:42 +0200

    Move constants into dedicated file

---
+++ Const.py
@@ -0,0 +1,2 @@
+hbar = 1.054_571_628e-34  # reducted const of Planck (J.s)
+m = 9.1e-31  # mass of electron (kg)
--- PaquetOndeGauss1d4J.py
+++ PaquetOndeGauss1d4J.py
@@ -1,8 +1,7 @@
 from matplotlib import pyplot as plt
 from numpy import arange, exp, imag, ndarray, pi, real
 
-hbar = 1.054_571_628e-34  # reducted const of Planck (J.s)
-m = 9.1e-31  # mass of electron (kg)
+from Const import hbar, m
 
 
 def GaussWP(k0: float, a: float, x: float, t: float) -> complex:
